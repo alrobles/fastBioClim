82cc3f384c79163040681a280cad53913ffde156
Angel Luis Robles Fernández <a.l.robles.fernandez@gmail.com>
2025-09-08 21:34:45 -0500

    Update bio_8.R
    reorediring the matrices index

--- R/bio_8.R
+++ R/bio_8.R
@@ -1,7 +1,8 @@
 #
 
-#' bio 8. Mean Temperature of Wettest Quarter. First we
-#' use a mobile window of three months to observer the 
+#' bio 8. Mean Temperature of Wettest Quarter.
+#' # First we use a mobile window of three months
+#' # to observer the 
 #' quarter of the year (a three month slice) with higher
 #' precipitation. After we get the index of this quarter
 #' we average the temperature of the three months of 
@@ -30,7 +31,9 @@ bio_8 <- function(pr, tas, filename = ""){
   b <- writeStart(out, filename, overwrite = TRUE, n = ncops);
   for (i in 1:b$n) {
     v_1 <- readValues(pr, b$row[i], b$nrows[i], 1, nc, TRUE)
-    r_1 <- fastBioClim::rcpp_parallel_which_max_quarter(mat_1 = v_1[ ,1:(ncol(v_1) - 2)], mat_2 = v_1[ ,2:(ncol(v_1) - 1)], mat_3 = v_1[ , 3:v_1[ ,2:ncol(v_1)]]  )
+    r_1 <- fastBioClim::rcpp_parallel_which_max_quarter(mat_1 = v_1[ ,1:(ncol(v_1) - 2)],
+                                                        mat_2 = v_1[ ,2:(ncol(v_1) - 1)],
+                                                        mat_3 = v_1[ ,3:ncol(v_1)]  )
     v_2 <- readValues(tas, b$row[i], b$nrows[i], 1, nc, TRUE);
     r <- fastBioClim::rcpp_get_max_quarter(maxQuarter = r_1, mat = v_2);
     writeValues(out, r, b$row[i], b$nrows[i]);
