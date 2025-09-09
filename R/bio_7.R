c91a7493ed1d5be2bde520634845891992397072
Angel Luis Robles Fernández <a.l.robles.fernandez@gmail.com>
2025-09-08 19:53:56 -0500

    update_bio7
    change r2 for r_2 in line 32

--- R/bio_7.R
+++ R/bio_7.R
@@ -25,9 +25,11 @@ bio_7 <- function(tasmax, tasmin, filename = ""){
   for (i in 1:b$n) {
     v_1 <- readValues(tasmax, b$row[i], b$nrows[i], 1, nc, TRUE)
     r_1 <- fastBioClim::rcpp_parallel_which_max_row(v_1)
+    
     v_2 <- readValues(tasmin, b$row[i], b$nrows[i], 1, nc, TRUE)
     r_2 <- fastBioClim::rcpp_parallel_which_min_row(v_2)
-    r <- fastBioClim::rcpp_parallel_difference(r_1,  r2)
+    
+    r <- fastBioClim::rcpp_parallel_difference(r_1,  r_2)
     writeValues(out, r, b$row[i], b$nrows[i])
   }
   writeStop(out)
